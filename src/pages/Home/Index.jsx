8f9596191bbc33e4ac8f0f421ccfd75696821f2b
Hairul Ardhan Nawazaki <hanawazaki002@gmail.com>
2023-09-21 18:34:11 +0700

    finishing mini project

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,35 +1,60 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ListTask from "../../components/ListTask";
-import Spinner from "../../components/Spinner";
-import axios from "axios";
 
 export default function Index() {
 
-  const [data, setData] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  const [data, setData] = useState([
 
-  const fetchData = async () => {
-    try {
-      setIsLoading(true)
-      await axios.get('http://localhost:3000/tasks')
-        .then((response) => {
-          if (response.data) {
-            setData(response.data);
-            console.log(response.data)
-            setIsLoading(false);
-          } else {
-            throw new Error('Respon tidak memiliki data JSON.');
-          }
-        })
-    } catch (err) {
-      console.log(err)
-      setIsLoading(false)
-    }
+  ])
+  const [newTask, setNewtask] = useState("")
+
+  const handleDelete = (id) => {
+    console.log('delete', id)
+    setData(prevData => {
+      return prevData.filter((item) => id !== item.id
+
+
+        // {
+
+        //   // const deletedidx = id !== item.id
+
+        //   // const currentData = JSON.parse(localStorage.getItem('tasks')) || []
+
+        //   // if (deletedidx !== -1) {
+        //   //   currentData.splice(deletedidx, 1)
+        //   // }
+
+        //   // localStorage.setItem("tasks", JSON.stringify(currentData))
+
+        // }
+
+      )
+    })
   }
 
-  useEffect(() => {
-    fetchData()
-  }, [])
+  const handleKeyUp = async (e) => {
+    if (e.key === 'Enter') {
+      addTask()
+    }
+  }
+  const handleNewTask = (e) => {
+    setNewtask(e.target.value)
+  }
+
+  const addTask = () => {
+    const newtask = {
+      id: data.length + 1,
+      taskname: newTask,
+      isDone: false
+    }
+
+    const newData = [...data]
+
+    newData.push(newtask)
+
+    setData(newData)
+    setNewtask("")
+  }
 
   return (
     <main>
@@ -38,25 +63,23 @@ export default function Index() {
           Task List
         </h1>
         <div className="input mx-auto flex justify-center w-[300px] mb-6">
-          <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 focus:outline-none " placeholder="what are we going to do?" />
+          <input value={newTask} onKeyUp={handleKeyUp} onChange={handleNewTask} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 focus:outline-none " placeholder="what are we going to do?" />
         </div>
         <div className="mx-[200px] border rounded-lg p-8">
           {
-            isLoading ? (
-              <div className="flex justify-center items-center w-full">
-                <Spinner />
-              </div>
-            ) : (
-
+            data.length ?
               data && data.map((item) => {
                 return (
 
                   <>
-                    <ListTask item={item} key={item.id} />
+                    <ListTask item={item} key={item.id} handleDelete={handleDelete} />
                   </>
                 )
-              })
-            )
+              }) :
+
+              <>
+                <h1 className="flex justify-center font-mono text-gray-400">no data</h1>
+              </>
           }
         </div>
       </div>
